a9f57b56730b72db1889b5692a07325a436170f5
Stuart Smith <stuart.ingersoll-smith@cherrythinking.com>
2019-08-28 20:52:48 +0100

    adding the data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import { FaBatteryEmpty, FaBatteryFull, FaBatteryHalf, FaBatteryQuarter, FaBatteryThreeQuarters } from 'react-icons/fa';
 import { IconContext } from "react-icons";
 import styled from 'styled-components';
-  
+import { data } from './data';
+
 // Create a Wrapper component that'll render a <section> tag with some styles
 const Wrapper = styled.section`
   padding: 4em;
@@ -29,25 +30,25 @@ function App() {
     <div>
       <ExperienceStyles><h1>Experience</h1></ExperienceStyles>
       <Wrapper>
-      <IconContext.Provider value={{ className: "global-class-name", size: "5em" }}>
-        <BatteryStyles>
-        <FaBatteryEmpty /> <TextStyles>0 years</TextStyles>
-        </BatteryStyles> 
-        <BatteryStyles>
-        <FaBatteryQuarter /> <TextStyles>1 years</TextStyles>
-        </BatteryStyles>
-        <BatteryStyles>
-        <FaBatteryHalf /> <TextStyles>2 years</TextStyles>
-        </BatteryStyles>
-        <BatteryStyles>
-        <FaBatteryThreeQuarters /> <TextStyles>3 years</TextStyles>
-        </BatteryStyles>
-        <BatteryStyles> 
-        <FaBatteryFull /> <TextStyles>4 years</TextStyles>
-        </BatteryStyles>
+        <IconContext.Provider value={{ className: "global-class-name", size: "5em" }}>
+          <BatteryStyles>
+            <FaBatteryEmpty /> <TextStyles>0 years</TextStyles>
+          </BatteryStyles>
+          <BatteryStyles>
+            <FaBatteryQuarter /> <TextStyles>1 years</TextStyles>
+          </BatteryStyles>
+          <BatteryStyles>
+            <FaBatteryHalf /> <TextStyles>2 years</TextStyles>
+          </BatteryStyles>
+          <BatteryStyles>
+            <FaBatteryThreeQuarters /> <TextStyles>3 years</TextStyles>
+          </BatteryStyles>
+          <BatteryStyles>
+            <FaBatteryFull /> <TextStyles>4 years</TextStyles>
+          </BatteryStyles>
         </IconContext.Provider>
-        </Wrapper>
-        </div>
+      </Wrapper>
+    </div>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const data = {
+    0: {
+        title: "0 years",
+        description: "I decided to"
+    },
+    1: {
+        title: "1 years",
+        description: "I decided to"
+    },
+    2: {
+        title: "2 years",
+        description: "I decided to"
+    },
+    3: {
+        title: "3 years",
+        description: "I decided to"
+    },
+    4: {
+        title: "4 years",
+        description: "I decided to"
+    }
+}
